f24f7fc4b995dd81c5f69c3555344d6fb05d9ae9
张佳宾 <victorzhang@vip.163.com>
2016-06-22 15:44:50 +0800

    add Queue.playground and README

---
+++ Queue/Queue.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+public struct Queue<T>{
+    var array: Array = [T]()
+    
+    public var count: Int {
+        return array.count
+    }
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+
+    public mutating func enqueue(t: T) {
+        array.append(t)
+    }
+    
+    public func dequeue() -> T? {
+        return array.first
+    }
+    
+    public mutating func peek() ->T? {
+        if isEmpty {
+            return nil
+        }else {
+            return array.removeFirst()
+        }
+    }
+}
